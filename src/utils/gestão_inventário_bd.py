b6290454807d6b58d09806d733830d294d95835b
Hugo Oliveira <hg.oliveira2ds@gmail.com>
2024-07-25 12:13:48 -0300

    Adicionando módulo da classe 'GestãoInventárioBD'

---
+++ src/utils/gestão_inventário_bd.py
@@ -0,0 +1,29 @@
+import mysql.connector
+
+class GestãoInventárioBD():
+    def __init__(self, host="localhost", user="root", password=""):
+            self.host = host
+            self.user = user
+            self.password = password
+            self.conexao = None
+            self.cursor = None
+            self.conectar_inventário()
+
+    def conectar_inventário(self):
+        try:
+            self.conexão = mysql.connector.connect(
+                host=self.host,
+                user=self.user,
+                password=self.password
+            )
+
+            self.cursor = self.conexão.cursor()
+            self.cursor.execute("CREATE DATABASE IF NOT EXISTS gestao_inventario")
+            print("Database 'gestao_inventario' criada!")
+            self.cursor.execute("USE gestao_inventario")
+            print("Database 'gestao_inventario' em uso!")
+
+            self.conexão.commit()
+
+        except mysql.connector.Error as e:
+            print(f"Erro ao conectar ao MySQL: {e}")
