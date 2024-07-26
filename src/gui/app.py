964a1ee03009c040b4c6404d7b4516be83f38cdb
Hugo Oliveira <hg.oliveira2ds@gmail.com>
2024-07-26 11:57:56 -0300

    Feat barra lateral

--- src/gui/app.py
+++ src/gui/app.py
@@ -1,43 +1,68 @@
 import flet as ft
 
-def main(page: ft.Page):
+def main(page):
     page.title = "Gestão de Inventário"
     page.window_min_width = 400
     page.window_min_height = 600
 
-    navRail = ft.NavigationRail(
-        selected_index=0,
-        label_type=ft.NavigationRailLabelType.ALL,
-        extended=True,
-        min_width=100,
-        min_extended_width=220,
-        group_alignment=-0.6,
-        destinations=[
-            ft.NavigationRailDestination(
-                icon=ft.icons.HOME, label="Início"
+    page_width = page.width
+
+    # Função para manipular o clique do botão
+    def on_button_click(index):
+        print(f"Destino selecionado: {index}")
+
+    # Itens de navegação
+    nav_items = [
+        {"icon": ft.icons.HOME, "label": "Início"},
+        {"icon": ft.icons.INVENTORY, "label": "Estoque"},
+        {"icon": ft.icons.SELL, "label": "Vendas"},
+        {"icon": ft.icons.FACTORY, "label": "Fornecedores"},
+        {"icon": ft.icons.SHOW_CHART, "label": "Relatório"}
+    ]
+
+    # Criar uma lista de botões com ícones e textos
+    nav_buttons = [
+        ft.IconButton(
+            content=ft.Row(
+                controls=[
+                    ft.Icon(item["icon"], size=24),  # Ajuste o tamanho do ícone conforme necessário
+                    ft.Text(value=item["label"], size=20)  # Ajuste o tamanho da fonte aqui
+                ],
+                alignment=ft.MainAxisAlignment.START
             ),
-            ft.NavigationRailDestination(
-                icon=ft.icons.INVENTORY, label="Estoque"
-            ),
-            ft.NavigationRailDestination(
-                icon=ft.icons.SELL, label="Vendas"
-            ),
-            ft.NavigationRailDestination(
-                icon=ft.icons.FACTORY, label="Fornecedores"
-            ),
-            ft.NavigationRailDestination(
-                icon=ft.icons.SHOW_CHART, label="Relatório"
+            on_click=lambda e, idx=i: on_button_click(idx),
+            style=ft.ButtonStyle(
+                padding=ft.Padding(top=10, right=10, bottom=10, left=25),
+                color=ft.colors.WHITE70
             )
-        ],
-        on_change=lambda e: print("Destino selecionado: ", e.control.selected_index)
-    )
+        )
+        for i, item in enumerate(nav_items)
+    ]
 
     page.add(
         ft.Row(
-            [
-                navRail,
-                ft.VerticalDivider(width=1),
-                ft.Column([ft.Text("Body")], alignment=ft.MainAxisAlignment.START, expand=True)
+            controls=[
+                ft.Container(
+                    bgcolor=ft.colors.DEEP_PURPLE_800,
+                    margin=ft.Margin(top=-10, bottom=-10, left=-10, right=-10),
+                    width=page.width*0.25,
+                    expand=False,
+                    content=ft.Container(
+                        bgcolor=ft.colors.DEEP_PURPLE_800,
+                        margin=ft.Margin(top=80, bottom=-10, left=20, right=20),
+                        content=ft.Column(
+                            controls=nav_buttons,
+                            spacing=15,
+                            alignment=ft.MainAxisAlignment.START,
+                            horizontal_alignment=ft.CrossAxisAlignment.START
+                        )
+                    )
+                ),
+                ft.Container(
+                    bgcolor=ft.colors.BLACK38,
+                    margin=ft.Margin(top=-10, bottom=-10, left=0, right=-10),
+                    expand=True
+                )
             ],
             expand=True
         )
